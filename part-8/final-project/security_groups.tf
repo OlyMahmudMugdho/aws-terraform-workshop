5a50b51764cc70beaf4a844458dcad5bd7ad50dc
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2025-04-02 22:45:30 +0600

    rule for HTTP from ALB to EC2

--- part-8/final-project/security_groups.tf
+++ part-8/final-project/security_groups.tf
@@ -35,6 +35,12 @@ resource "aws_security_group" "ec2_security_group" {
     protocol        = "tcp"
     security_groups = [aws_security_group.rds_security_group.id]
   }
+  ingress {  # rule for HTTP from ALB to EC2
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_security_group.id]
+  }
   egress {
     from_port   = 0
     to_port     = 0
